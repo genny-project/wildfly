8852f47cadf2e3e1f337131521059edb1672b610
Jian Li <hbjoylee@gmail.com>
2020-10-21 05:59:27 +0000

    clean up build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -8,7 +8,3 @@ fi
 
 docker build  --no-cache -t gennyproject/wildfly:${version} . 
 #docker build -f DockerfileJRebel  --no-cache -t gennyproject/wildfly:jrebel . 
-
-if [ -z "${1}" ]; then
-   docker tag gennyproject/wildfly:latest gennyproject/wildfly:v7.0.0
-fi
